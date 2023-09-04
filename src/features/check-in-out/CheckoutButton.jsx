88c40550cfdb0c3afa0bf013b5c81decfd851622
Liufat <dd5672003@gmail.com>
2023-09-04 15:16:47 +0800

    [新增]dashboard-today's activities-check in / out

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,20 @@
 import Button from "../../ui/Button";
+import { useCheckout } from "./useCheckout";
 
 function CheckoutButton({ bookingId }) {
+  const { checkout, isCheckingOut } = useCheckout();
+
+  const handleClick = () => {
+    checkout(bookingId);
+  };
+
   return (
-    <Button variation="primary" size="small">
+    <Button
+      variation="primary"
+      size="small"
+      onClick={handleClick}
+      disabled={isCheckingOut}
+    >
       Check out
     </Button>
   );
